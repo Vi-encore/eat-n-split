e30be398b7f28a55bccbfa263de2f15b8f9d2fcd
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-08-23 19:15:53 +0300

    basic stuff completed. File App was split into smaller ones. Problem: state of splitBill form does not clear when clicking on different friend.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import { useState } from "react";
+import { FormSplitBill } from "./FormSplitBill";
+import { FormAddFriend } from "./FormAddFriend";
+import { FriendsList } from "./FriendsList";
+import { Button } from "./Button";
+
 const initialFriends = [
   {
     id: 118836,
@@ -18,3 +24,59 @@ const initialFriends = [
     balance: 0,
   },
 ];
+
+export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends); //for demo, init state = []
+  const [selectedFriend, setSelectedFriend] = useState(null);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
+  function handleSelection(friend) {
+    setSelectedFriend((cur) => (cur?.id === friend.id ? null : friend));
+    setShowAddFriend(false);
+  }
+
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+
+    setSelectedFriend(null);
+  }
+
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList
+          friends={friends}
+          onSelection={handleSelection}
+          selectedFriend={selectedFriend}
+        />
+
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add friend"}
+        </Button>
+      </div>
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
+        />
+      )}
+    </div>
+  );
+}
